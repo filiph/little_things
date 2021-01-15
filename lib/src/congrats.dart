175c630ddf7a9e2c23f494f76e4a31813ebe598f
Filip Hracek <filip.hracek@gmail.com>
2021-01-15 13:30:48 -0800

    Animating everything at once

--- lib/src/congrats.dart
+++ lib/src/congrats.dart
@@ -38,13 +38,28 @@ class _CardContents extends StatefulWidget {
   _CardContentsState createState() => _CardContentsState();
 }
 
-class _CardContentsState extends State<_CardContents> {
+class _CardContentsState extends State<_CardContents>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   void didUpdateWidget(covariant _CardContents oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (!oldWidget.isDone && widget.isDone) {
-      // TODO: start the showing animation
-      print('starting show animation');
+      _controller.duration = const Duration(milliseconds: 2000);
+      _controller.forward(from: 0);
     }
   }
 
@@ -54,20 +69,24 @@ class _CardContentsState extends State<_CardContents> {
 
     return Column(
       children: [
-        Text('🎉', style: TextStyle(fontSize: 50)),
+        ScaleTransition(
+          scale: CurvedAnimation(
+            curve: Curves.elasticOut,
+            parent: _controller,
+          ),
+          child: Text('🎉', style: TextStyle(fontSize: 50)),
+        ),
         SizedBox(height: 20),
         Text(
           'Congratulations!',
-          style: theme
-              .textTheme
-              .headline4
-              .copyWith(fontSize: 30),
+          style: theme.textTheme.headline4.copyWith(fontSize: 30),
         ),
         Padding(
           padding: const EdgeInsets.only(top: 20, bottom: 30),
-          child: Text(
+          child: TrackedOutText(
             'You have successfully completed watching this talk. '
             'You get 50 completely meaningless virtual points!',
+            _controller,
             textAlign: TextAlign.center,
             style: theme.textTheme.bodyText2.copyWith(
               fontSize: 14,
@@ -75,6 +94,63 @@ class _CardContentsState extends State<_CardContents> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class TrackedOutText extends StatefulWidget {
+  final String text;
+
+  final List<String> _slices;
+
+  final Animation<double> progress;
+
+  final TextAlign textAlign;
+  final TextStyle style;
+
+  TrackedOutText(
+    this.text,
+    this.progress, {
+    this.textAlign,
+    this.style,
+  }) : _slices = _generateSlices(text).toList(growable: false);
+
+  static Iterable<String> _generateSlices(String text) sync* {
+    const step = 2;
+    var i = 0;
+    for (; i < text.length - step; i += step) {
+      yield text.substring(i, i + step);
+    }
+    yield text.substring(i);
+  }
+
+  @override
+  _TrackedOutTextState createState() => _TrackedOutTextState();
+}
+
+class _TrackedOutTextState extends State<TrackedOutText> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.progress,
+      builder: (BuildContext context, Widget child) {
+        return Text.rich(
+          TextSpan(
+            children: [
+              // XXX: This should be cached instead of recreating everything
+              // on each frame.
+              for (var i = 0; i < widget._slices.length; i++)
+                TextSpan(
+                    text: widget._slices[i],
+                    style: (i / widget._slices.length <= widget.progress.value)
+                        ? null
+                        : TextStyle(color: Colors.transparent))
+            ],
+          ),
+          textAlign: widget.textAlign,
+          style: widget.style,
+        );
+      },
     );
   }
 }
