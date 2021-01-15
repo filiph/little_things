178007ede3f745355e2a27eb651d94b5aa697ed9
Filip Hracek <filip.hracek@gmail.com>
2021-01-15 12:49:09 -0800

    Add meditation image

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -15,34 +15,53 @@ class MyAgendaList extends StatelessWidget {
     return Stack(
       children: [
         Scaffold(
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 80, right: 80),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text('My agenda', style: theme.textTheme.headline4),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      'These are the topics I’d like to discuss today.',
-                      style: theme.textTheme.subtitle1,
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, bottom: 15),
-                    child: Divider(
-                      thickness: 2,
-                      color: theme.accentColor,
-                    ),
-                  ),
-                  for (final item in agenda.items) AgendaListItem(item),
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF96E3FF),
+                  Color(0xFF9EECFF),
+                  Color(0xFF9FEBFF),
+                  Color(0xFF9FEEFF),
+                  Color(0xFF9FECFF),
                 ],
+              ),
+              image: DecorationImage(
+                image: AssetImage('assets/meditation.jpg'),
+                alignment: Alignment.bottomCenter,
+              ),
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 80, right: 80, bottom: 180),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child:
+                          Text('My agenda', style: theme.textTheme.headline4),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        'These are the topics I’d like to discuss today.',
+                        style: theme.textTheme.subtitle1,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 15),
+                      child: Divider(
+                        thickness: 2,
+                        color: theme.accentColor,
+                      ),
+                    ),
+                    for (final item in agenda.items) AgendaListItem(item),
+                  ],
+                ),
               ),
             ),
           ),
